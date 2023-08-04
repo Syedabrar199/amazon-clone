3db528dc9f9a9edcce7abc7e2ac454988dcd2825
Syed_abrar <abrarulhaqsyed3@gmail.com>
2023-08-04 16:35:10 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./Login";
 import Checkout from "./Checkout";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Header />} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/login" element={<Login />} />
     </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      {/* {logo} */}
+
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+      {/* {searchbox} */}
+      {/* {3 links} */}
+      {/* {basket icon with nmbr} */}
+    </nav>
+  );
+};
+
+export default Header;
